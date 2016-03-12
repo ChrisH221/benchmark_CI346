cace67a4cf69a0e75201c58d9331f3ed6264b90c
Chris <C.Howell2@uni.brighton.ac.uk>
2016-03-12 12:46:20 +0000

    Implemented comparator for implicit.
    Finished

--- src/benchImplicit.java
+++ src/benchImplicit.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
-import java.util.Optional;
-import java.util.concurrent.SynchronousQueue;
+import java.util.Comparator;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -14,13 +14,11 @@ import java.util.concurrent.TimeUnit;
  */
 public class benchImplicit {
 
-    ArrayList arr = new ArrayList();
+    ArrayList arr = new ArrayList<Integer>();
     ArrayList arr2 = new ArrayList();
     public benchImplicit(ArrayList arr){
 
     this.arr = arr;
-   
-
 
     }
 
@@ -29,7 +27,9 @@ public class benchImplicit {
         long start = System.nanoTime();
 
 
-        arr.parallelStream().sorted();
+
+            arr.parallelStream().sorted(new BubblesortListSorter(arr));
+
 
         long finish = System.nanoTime();
 
@@ -37,6 +37,36 @@ public class benchImplicit {
         return seconds;
     }
 
+
+    @SuppressWarnings("unchecked")
+    public  class BubblesortListSorter implements Comparator<Integer> {
+
+        public BubblesortListSorter(ArrayList<Integer> arr){
+
+            sort(arr);
+
+        }
+
+        public int compare(Integer o1, Integer o2) {
+           if(o1 < o2) return 01;
+           else return 02;
+        }
+
+        List<Integer> sort(List<Integer> arr){
+            int temp;
+            for( int y=0; y<arr.size()-1; y++ ) {
+                for ( int z=y+1; z<arr.size(); z++ ){
+                    if(compare(arr.get(z),arr.get(y) ) == 1 ) {
+                        temp = arr.get(y);
+                        arr.set(y,arr.get(z));
+                        arr.set(z,temp);
+                    }
+                }
+            }
+
+            return arr;
+        }
+    }
 
     }
 
